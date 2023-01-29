db5bfee02a38390af0ad4313e39357a2950aa9a0
TROFFik <109242655+TROFFik@users.noreply.github.com>
2023-01-29 23:41:46 +0300

    Made movement
    - Made base MVC classes
    - Made the player movement to the mouse click point
    - Made the player stop if he was clicked

---
+++ Assets/Scripts/Player/PlayerControler.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerView))]
+public class PlayerControler : MonoBehaviour
+{
+    [Tooltip("Player move speed")]
+    [SerializeField] private float _speed; 
+
+    private PlayerModel _playerModel;
+    private PlayerView _playerView;
+
+    private Vector3 _target—oordinates;
+    private void Start()
+    {
+        GetPlayerComponents();
+    }
+
+    private void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Input—alculations();
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, _target—oordinates, _playerModel.Speed);
+    }
+
+    private void GetPlayerComponents()
+    {
+        _playerModel = new PlayerModel();
+        _playerView = GetComponent<PlayerView>();
+
+        _playerModel.Speed = _speed;
+    }
+
+    private void Input—alculations()
+    {
+        
+        Touch touch = Input.GetTouch(0);
+        Ray ray = Camera.main.ScreenPointToRay(touch.position);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            var hitPlayer = hit.collider.gameObject.GetComponent<PlayerControler>();
+            if (hitPlayer != null)
+            {
+                _target—oordinates = transform.position;
+            }
+            else
+            _target—oordinates = new Vector3(hit.point.x, 0, hit.point.z);
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerModel.cs
@@ -0,0 +1,10 @@
+public class PlayerModel
+{
+    private float _speed;
+
+    public float Speed
+    {
+        get { return _speed; }
+        set { _speed = value; }
+    }
+}
